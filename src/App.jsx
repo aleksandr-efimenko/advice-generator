e9364465ee6d5c0fc3239cc6f26c65b155271728
androidblog <androidblog@yandex.ru>
2023-03-27 17:14:12 +0400

    create styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [advice, setAdvice] = useState('')
+  const [advice, setAdvice] = useState("Hello, world!");
   const [num, setNum] = useState(0);
 
   return (
     <div className="App">
-      <h1>Advice {num}</h1>
+      <h1 className="advice-header">Advice #{num}</h1>
+      <div className="advice-quote"><q>{advice}</q></div>
+      <div className="divider"></div>
+      <div className="btn-container">
+        <button className="btn" onClick={() => setNum(num + 1)}></button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
