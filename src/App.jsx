d06e5fa9e911a3de9874bee633eb279e55a376ac
androidblog <androidblog@yandex.ru>
2023-03-27 16:24:49 +0400

    create basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [advice, setAdvice] = useState('')
+  const [num, setNum] = useState(0);
 
   return (
     <div className="App">
-
+      <h1>Advice {num}</h1>
     </div>
   )
 }
